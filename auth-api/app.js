924bee599fb4ed7f0d0d97e98a5ffc55b90e4a97
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-07-27 20:26:54 -0300

    feat: adjusting auth-api

--- auth-api/app.js
+++ auth-api/app.js
@@ -1,22 +1,17 @@
 import express from "express";
 
-import * as db from "./scr/config/db/initialData.js";
-
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
 
-db.createInitialData();
-
-app.get('/api/status', (req, res) =>{
+app.get('/api/status', (req, res) => {
     return res.status(200).json({
         service: "Auth-API",
         status: "up",
-        httpStatus: 200,
-
-    });
-});
+        httpStatus: 200
+    })
+})
 
 app.listen(PORT, () => {
-    console.info(`Server started successfully at port ${PORT}`)
+    console.log(`Server started sucessfully at port ${PORT}`);
 })
