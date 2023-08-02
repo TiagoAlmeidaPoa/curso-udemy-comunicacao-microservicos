866cbfc44f09bca64442798fe88d34dcacfb8bd0
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-08-02 19:45:28 -0300

    feat: developing authentication with jwt

---
+++ auth-api/src/config/constants/secrets.js
@@ -0,0 +1,4 @@
+const env = process.env;
+// auth-api-secret-dev-123456
+export const API_SECRET = env.API_SECRET ? env.API_SECRET : "YXV0aC1hcGktc2VjcmV0LWRldi0xMjM0NTY=";
+
--- auth-api/src/modules/service/UserService.js
+++ auth-api/src/modules/service/UserService.js
@@ -1,6 +1,12 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 import userRepository from "../repository/UserRepository.js";
-import * as HttpStatus from "../../config/constants/HttpStatus.js"
 import UserException from "../user/exception/UserException.js";
+import User from "../user/model/User.js";
+import UserRepository from "../repository/UserRepository.js";
+import * as HttpStatus from "../../config/constants/HttpStatus.js"
+import * as secrets from "../../config/constants/secrets.js"
 
 class UserService {
 
@@ -35,6 +41,40 @@ class UserService {
     validateUserNotFound(user) {
         if (!user) {
             throw new Error( HttpStatus.BAD_REQUEST, "User was not found !" );
+        }
+    }
+
+    async getAccessToken(req) {
+        try {
+            const {email, password} = req.body;
+            this.validateAccessTokenData(email, password);        
+            let user = await UserRepository.findByEmail(email); 
+            this.validateUserNotFound(user);
+            await this.validatePassword(password, user.password);
+            const authUser = { d: user.id, name: user.name, email: user.email };
+            const accessToken = jwt.sign({authUser}, secrets.API_SECRET ,{expiresIn: '1d'});
+            return {
+                status: HttpStatus.SUCCESS,
+                accessToken
+            }
+        } catch (error) {
+            return {
+                status: error.status ? error.status : HttpStatus.INTERNAL_SERVER_ERROR,
+                message: error.message
+            }
+        }
+
+    }
+
+    validateAccessTokenData(email, password) {
+        if(!email || !password) {
+            throw new UserException(HttpStatus.UNAUTHORIZED, "Email and password must bee informed.");
+        }
+    }
+
+    async validatePassword(password, hashPassword) {
+        if (!await bcrypt.compare(password, hashPassword)) {
+            throw new UserException(HttpStatus.UNAUTHORIZED, "Password doesn't match.")
         }
     }
 
